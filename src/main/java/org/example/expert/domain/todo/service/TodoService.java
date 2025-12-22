e7ce624c287a9f442ac111778e7e282b8f80e029
이용준 <abc157898@gmail.com>
2025-12-22 13:53:52 +0900

    Lv8 - 완료 1. Querydsl 사용을 위한 의존성 주입 2. 레퍼지토리 및 클래스 추가 3. 메서드 변경

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -74,7 +74,7 @@ public class TodoService {
 
     @Transactional(readOnly = true)
     public TodoResponse getTodo(long todoId) {
-        Todo todo = todoRepository.findByIdWithUser(todoId)
+        Todo todo = todoRepository.getOneTodo(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
         User user = todo.getUser();
